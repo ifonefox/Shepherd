6dd813af0f7dbb86712a53831d2ae74ab82a9647
Sean Brody <ifonefox@gmail.com>
2015-09-12 20:56:29 -0400

    added test.rb

---
+++ test.rb
@@ -0,0 +1,56 @@
+names = ["Emma", "Noah", "Olivia", "Liam", "Alex"]
+$rand = Random.new
+$url = "sbrody.org"
+class Heart
+  attr_writer :countdown
+  attr_reader :thread
+  def initialize(name)
+    @countdown = 0
+    @name = name
+    @mutex = Mutex.new()
+  end
+  def update()
+    @mutex.synchronize do
+      if @countdown == 0
+        num = $rand.rand(55..65)
+        command = "curl '#{$url}:3000/post?bpm=#{num}&user=#{@name}' 2> /dev/null"
+        puts(command)
+        a = `#{command}`
+      else
+        @countdown = @countdown - 1
+        command = "curl '#{$url}:3000/post?bpm=90&user=#{@name}' 2> /dev/null"
+        puts(command)
+        a = `#{command}`
+      end
+    end
+  end
+  def start()
+    @thread = Thread.new do
+      while true
+        self.update()
+        sleep 5
+      end
+    end
+  end
+  def err()
+    @mutex.synchronize do
+      @countdown = 5
+    end
+  end
+end
+hearts = []
+for i in names 
+  hearts.push(Heart.new(i))
+end
+for heart in hearts
+  heart.start()
+end
+Thread.new{
+  while true
+    hearts[$rand.rand(0..hearts.length)].err()
+    sleep 60
+  end
+}
+for heart in hearts
+  heart.thread.join()
+end
